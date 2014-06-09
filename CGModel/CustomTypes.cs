6060fcb888bc029df66a696c176f17422dde8b2f
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2014-06-09 13:56:30 +0200

    Great unification (old C#3.0 projects) New commands ExportFile, ImportFile Standalone Creator implementation 75%

--- CGModel/CustomTypes.cs
+++ CGModel/CustomTypes.cs
@@ -13,6 +13,39 @@ namespace TK.BaseLib.CGModel
             get {return _elementInfoName;}
         }
 
+        public static string ParamTypeName
+        {
+            get { return "tk_type"; }
+        }
+        public static string ParamRuleName
+        {
+            get { return "tk_guideRule"; }
+        }
+        public static string ParamPlaceHolderName
+        {
+            get { return "tk_placeHolder"; }
+        }
+        public static string ParamUpVName
+        {
+            get { return "tk_upVReference"; }
+        }
+        public static string ParamTargetName
+        {
+            get { return "tk_target"; }
+        }
+        public static string ParamInvertName
+        {
+            get { return "tk_invert"; }
+        }
+        public static string ParamInvertUpName
+        {
+            get { return "tk_invertUp"; }
+        }
+        public static string ParamGuideParentName
+        {
+            get { return "tk_guideParent"; }
+        }
+
         public static string[] ElementInfoTypes
         {
             get { return _elementInfoTypes; }
@@ -22,10 +55,21 @@ namespace TK.BaseLib.CGModel
         {
             get { return _guideRules; }
         }
-    }
 
-    public enum RigObjectType
-    { Model, Root, Input, Output, Null, Control, Deform, PlaceHolder }
+        public static int GuideRule(string inGuideRule)
+        {
+            int rule = 0;
+            foreach(string strRule in _guideRules)
+            {
+                if(strRule == inGuideRule)
+                {
+                    return rule;
+                }
+                rule++;
+            }
+            return 0;
+        }
+    }
 
     public enum AffectType
     { NoEffect, True, False }
---
+++ Checking/CheckHelper.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Xml.Serialization;
+
+namespace TK.BaseLib.Checking
+{
+    public static class CheckHelper
+    {
+        static XmlSerializer serializer = null;
+
+        /// <summary>
+        /// Serialize the Data to a file
+        /// </summary>
+        /// <param name="inPath">Path to save the file to</param>
+        /// <param name="inOverWrite">OverWrite if exists</param>
+        /// <returns>True in case of success</returns>
+        public static bool Save(CheckList inCheckList, string inPath, bool inOverWrite)
+        {
+            FileInfo ThisFileInfo = new FileInfo(inPath);
+
+            if (inOverWrite || !(ThisFileInfo.Exists))
+            {
+                if (serializer == null)
+                {
+                    serializer = new XmlSerializer(typeof(CheckList));
+                }
+
+                DirectoryInfo parentDir = ThisFileInfo.Directory;
+
+                if (!parentDir.Exists)
+                {
+                    parentDir.Create();
+                }
+
+                if (ThisFileInfo.Exists)
+                {
+                    ThisFileInfo.Delete();
+                }
+
+                FileStream stream = ThisFileInfo.OpenWrite();
+
+                try
+                {
+                    serializer.Serialize(stream, inCheckList);
+                    stream.Close();
+                    return true;
+                }
+                catch (Exception)
+                {
+                    if (stream != null)
+                    {
+                        stream.Close();
+                    }
+                }
+            }
+
+            return false;
+        }
+
+        /// <summary>
+        /// Deserialize the Data to a file, Always overwriting
+        /// </summary>
+        /// <param name="inPath">Path to load the file from</param>
+        /// <returns>True in case of Success</returns>
+        public static CheckList Load(string inPath)
+        {
+            FileInfo ThisFileInfo = new FileInfo(inPath);
+
+            if (ThisFileInfo.Exists)
+            {
+                if (serializer == null)
+                {
+                    serializer = new XmlSerializer(typeof(CheckList));
+                }
+
+                FileStream stream = ThisFileInfo.OpenRead();
+
+                try
+                {
+                    CheckList loaded = (CheckList)serializer.Deserialize(stream);
+                    stream.Close();
+                    return loaded;
+                }
+                catch (Exception)
+                {
+                    if (stream != null)
+                    {
+                        stream.Close();
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+}
