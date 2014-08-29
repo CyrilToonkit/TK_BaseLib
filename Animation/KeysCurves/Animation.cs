0654b8683c5face2741dc658321d65ec501fbf0c
Cyril Gibaud <cyril.gibaud@toonkit-studio.com>
2014-08-29 18:45:07 +0200

    New files from previous commit

---
+++ Animation/KeysCurves/Animation.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TK.BaseLib.Animation.KeysCurves
+{
+    public class Animation : SaveableData
+    {
+        string _name = "unkwnown";
+        public string Name
+        {
+            get { return _name; }
+            set { _name = value; }
+        }
+
+        ActionTypes _type = ActionTypes.Anim;
+        public ActionTypes Type
+        {
+            get { return _type; }
+            set { _type = value; }
+        }
+
+        List<AnimCurve> _curves = new List<AnimCurve>();
+        public List<AnimCurve> Curves
+        {
+            get { return _curves; }
+            set { _curves = value; }
+        }
+
+        public override void Clone(SaveableData Data)
+        {
+            Animation anim = Data as Animation;
+
+            if (anim != null)
+            {
+                _name = anim.Name;
+                _curves = anim.Curves;
+                _type = anim.Type;
+            }
+        }
+
+ 
+    }
+}
